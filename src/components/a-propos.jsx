87a39b49b100e6ef4c1983553bc9d79cb133e9a8
Vianney-Gln <vgeloen@gmail.com>
2021-11-27 22:23:52 +0100

    import et mise en place de l'image avatar dans le a propos

--- src/components/a-propos.jsx
+++ src/components/a-propos.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 //style
 import "../styles/a-propos.scss";
+import avatar from "../images/avatar-vianney-400.png";
 
 const Apropos = () => {
   return (
-    <div classNmae="apropos">
-      <p>Qui suis-je?</p>
+    <div className="apropos">
+      <p>QUI SUIS - JE?</p>
+      <div className="container-avatar">
+        <img src={avatar} alt="avatar-vianney" />
+      </div>
     </div>
   );
 };
