fada78e17d53d2cb34bcd3b0c3f88581c8290af0
Vianney-Gln <vgeloen@gmail.com>
2021-11-27 21:21:58 +0100

    ajout de texte d'accroche au milieu du heros + bouton a propos de moi

--- src/components/heros.jsx
+++ src/components/heros.jsx
@@ -3,7 +3,15 @@ import React from "react";
 import "../styles/heros.scss";
 
 const Heros = () => {
-  return <div className="heros"></div>;
+  return (
+    <div className="heros">
+      <div className="container-fond-transparent">
+        <h2>Vianney Geloen</h2>
+        <p>Développeur web - junior</p>
+      </div>
+      <button type="button">A propos de moi</button>
+    </div>
+  );
 };
 
 export default Heros;
