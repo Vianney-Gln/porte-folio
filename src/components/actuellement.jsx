50863ec191a5fd05fba93aee0d66694e23d15bfa
Vianney-Gln <vgeloen@gmail.com>
2021-11-27 23:53:38 +0100

    mise en place du background-image + texte par dessus

--- src/components/actuellement.jsx
+++ src/components/actuellement.jsx
@@ -3,7 +3,18 @@ import React from "react";
 import "../styles/actuellement.scss";
 
 const Actuellement = () => {
-  return <div className="actuellement"></div>;
+  return (
+    <div className="actuellement">
+      <p className="actuellement-gras">Actuellement :</p>
+      <br></br>
+      <br></br>
+      <p className="text-normal">
+        En formation développeur web React-js/Express-js chez<br></br>
+        <br></br>
+        <span>Wild Code School</span>
+      </p>
+    </div>
+  );
 };
 
 export default Actuellement;
