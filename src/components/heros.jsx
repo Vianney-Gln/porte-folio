2be811324f280008047cb8b3e193b95cb2b00033
Vianney-Gln <vgeloen@gmail.com>
2021-11-27 20:44:09 +0100

    creation d'un composant heros + heros.scss, heros placé dans le app

---
+++ src/components/heros.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+//style
+import "../styles/heros.scss";
+
+const Heros = () => {
+  return <div className="heros"></div>;
+};
+
+export default Heros;
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
-import "../styles/App.scss";
 import Header from "../components/header";
+import Heros from "../components/heros";
+//style
+import "../styles/App.scss";
 
 const App = () => {
   const [isOpen, setOpen] = useState(true);
   return (
     <div className="App">
       <Header toggle={isOpen} setToggle={setOpen} />
+      <Heros />
     </div>
   );
 };
