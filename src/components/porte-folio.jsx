3cae80943d085d2210aa6e39b7b56bb4e66dcc90
Vianney-Gln <vgeloen@gmail.com>
2021-11-28 19:46:15 +0100

    texte de présentation des projets écris et mis en forme

--- src/components/porte-folio.jsx
+++ src/components/porte-folio.jsx
@@ -6,6 +6,11 @@ const PorteFolio = () => {
   return (
     <div className="porteFolio" id="porte-folio">
       <h2>Porte folio</h2>
+      <p>
+        Vous trouverez ci dessous les projets réalisés pendant la formation.
+        <br></br>2 projets fictifs ont déjà été effectués, un troisième est en
+        cours et sera réalisé pour un vrais client!
+      </p>
     </div>
   );
 };
