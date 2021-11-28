d377c348b7aa3a57d6c213d4cd179e872479ee67
Vianney-Gln <vgeloen@gmail.com>
2021-11-28 20:01:53 +0100

    creation d'un fichier data qui recupere adresses liens et images des précédents projets sous forme de tableau d'objets

---
+++ src/data/projets.jsx
@@ -0,0 +1,19 @@
+import imagePanamWeb from "../images/panam-web.png";
+import imageFocus from "../images/focus.png";
+
+const projets = [
+  {
+    id: 1,
+    title: "Panam-Web",
+    lien: "https://guilhemcv.github.io/panam-web/",
+    image: imagePanamWeb,
+  },
+  {
+    id: 2,
+    title: "Focus",
+    lien: "https://focus-wcs.netlify.app/",
+    image: imageFocus,
+  },
+];
+
+export default projets;
