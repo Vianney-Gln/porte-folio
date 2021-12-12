f02b289019a2ee2a053d2392193a982bc4ff0396
Vianney-Gln <vgeloen@gmail.com>
2021-12-12 13:49:26 +0100

    competences responsiv ok

--- src/components/compétences.jsx
+++ src/components/compétences.jsx
@@ -17,51 +17,59 @@ const Competences = () => {
   return (
     <div className="competences" id="competence">
       <h2>Compétences</h2>
-      <p>
-        Actuellement encore en formation, mes compétences sont en constantes
-        évolutions. Pour l'instant elles sont principalement centrées sur
-        l'environnement Javascript :
-      </p>
+      <div className="description-competences">
+        <p>
+          Actuellement encore en formation, mes compétences sont en constantes
+          évolutions. Pour l'instant elles sont principalement centrées sur
+          l'environnement Javascript :
+        </p>
+      </div>
       <div className="section-langages">
-        <h4>Back end: </h4>
-        <ul className="liste">
-          <li>
-            <img src={nodeJs} alt="icon-nodeJs" />
-          </li>
-          <li>
-            <img src={php} alt="icon-php" />
-          </li>
-          <li>
-            <img src={mySql} alt="icon-mySql" />
-          </li>
-        </ul>
-        <h4>Front end: </h4>
-        <ul className="liste">
-          <li>
-            <img src={html5} alt="icon-html5" />
-          </li>
-          <li>
-            <img src={css3} alt="icon-css3" />
-          </li>
-          <li>
-            <img src={sass} alt="icon-sass" />
-          </li>
-          <li>
-            <img src={logoreact} alt="icon-react" />
-          </li>
-          <li>
-            <img src={javascript} alt="icon-javascript" />
-          </li>
-        </ul>
-        <h4>divers: </h4>
-        <ul className="liste-center">
-          <li>
-            <img src={git} alt="icon-git" />
-          </li>
-          <li>
-            <img src={bootstrap} alt="icon-bootstrap" />
-          </li>
-        </ul>
+        <div>
+          <h4>Back end: </h4>
+          <ul className="backend">
+            <li>
+              <img src={nodeJs} alt="icon-nodeJs" />
+            </li>
+            <li>
+              <img src={php} alt="icon-php" />
+            </li>
+            <li>
+              <img src={mySql} alt="icon-mySql" />
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h4>Front end: </h4>
+          <ul className="frontend">
+            <li>
+              <img src={html5} alt="icon-html5" />
+            </li>
+            <li>
+              <img src={css3} alt="icon-css3" />
+            </li>
+            <li>
+              <img src={sass} alt="icon-sass" />
+            </li>
+            <li>
+              <img src={logoreact} alt="icon-react" />
+            </li>
+            <li>
+              <img src={javascript} alt="icon-javascript" />
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h4>divers: </h4>
+          <ul className="divers">
+            <li>
+              <img src={git} alt="icon-git" />
+            </li>
+            <li>
+              <img src={bootstrap} alt="icon-bootstrap" />
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
