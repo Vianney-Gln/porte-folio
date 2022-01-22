83a24bca63a050e77904274785cccb4ab0bb452d
Vianney-Gln <vgeloen@gmail.com>
2022-01-22 15:07:33 +0100

    modif descriptif portfolio

--- src/components/porte-folio.jsx
+++ src/components/porte-folio.jsx
@@ -15,7 +15,9 @@ const PorteFolio = () => {
       <p className="descriptif-porteFolio">
         Vous trouverez ci dessous les projets réalisés pendant la formation.
         <br></br>2 projets fictifs ont déjà été effectués, un troisième est en
-        cours et sera réalisé pour un vrai client !
+        cours et sera réalisé pour un vrai client !<br></br>Vous trouverez
+        également un travail effectué lors de l'évènement Hackathon Manomano
+        réalisé les 20 et 21 Janvier 2022.
       </p>
       <div className="slide-container">
         <Slide>
