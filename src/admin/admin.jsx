ea5cb0a479aceb8ec8da1b7d43455b5e495eed61
Vianney-Gln <vgeloen@gmail.com>
2022-06-24 13:53:11 +0200

    modif css

--- src/admin/admin.jsx
+++ src/admin/admin.jsx
@@ -23,57 +23,61 @@ const Admin = () => {
   return (
     <div className="admin">
       <h1>Administration du portfolio</h1>
-      <form className="form-photo">
-        <h2>Changer la photo de l'introduction</h2>
-        <label htmlFor="file">
-          <input
-            type="file"
-            name="image"
-            onChange={(e) => setFile(e.target.files[0])}
-          ></input>
-        </label>
-        <button type="button" onClick={(e) => send(e)}>
-          send
-        </button>
-        {message && <p>{message}</p>}
-      </form>
-      <form className="form-text-intro">
-        <h2>Modification du texte d'introduction ou de actuellement </h2>
-        <label htmlFor="introduction">
-          <textarea placeholder="introduction" name="introduction"></textarea>
-        </label>
-        <label htmlFor="actually">
-          <input type="text" name="actually" placeholder="actually"></input>
-        </label>
-        <button type="button">valider</button>
-      </form>
-      <form className="form-projects">
-        <h2>Gestion des projets</h2>
-        <label htmlFor="name">
-          <input type="text" name="name" placeholder="nom du projet"></input>
-        </label>
-        <label htmlFor="url">
-          <input type="text" name="url" placeholder="url du site"></input>
-        </label>
-        <label htmlFor="image-url">
-          <input
-            type="text"
-            name="image-url"
-            placeholder="url image du projet"
-          ></input>
-        </label>
-        <label htmlFor="description">
-          <input
-            type="text"
-            name="description"
-            placeholder="description du projet"
-          ></input>
-        </label>
-        <label htmlFor="date">
-          <input type="text" name="date" placeholder="dates"></input>
-        </label>
-        <button type="button">valider</button>
-      </form>
+      <div className="container-form">
+        <form className="form-photo">
+          <h2>Changer la photo de l'introduction</h2>
+          <div className="change-button">
+            <label htmlFor="file">
+              <input
+                type="file"
+                name="image"
+                onChange={(e) => setFile(e.target.files[0])}
+              ></input>
+            </label>
+            <button type="button" onClick={(e) => send(e)}>
+              changer la photo
+            </button>
+          </div>
+          {message && <p>{message}</p>}
+        </form>
+        <form className="form-text-intro">
+          <h2>Modification du texte d'introduction ou de actuellement </h2>
+          <label htmlFor="introduction">
+            <textarea placeholder="introduction" name="introduction"></textarea>
+          </label>
+          <label htmlFor="actually">
+            <input type="text" name="actually" placeholder="actually"></input>
+          </label>
+          <button type="button">valider</button>
+        </form>
+        <form className="form-projects">
+          <h2>Gestion des projets</h2>
+          <label htmlFor="name">
+            <input type="text" name="name" placeholder="nom du projet"></input>
+          </label>
+          <label htmlFor="url">
+            <input type="text" name="url" placeholder="url du site"></input>
+          </label>
+          <label htmlFor="image-url">
+            <input
+              type="text"
+              name="image-url"
+              placeholder="url image du projet"
+            ></input>
+          </label>
+          <label htmlFor="description">
+            <input
+              type="text"
+              name="description"
+              placeholder="description du projet"
+            ></input>
+          </label>
+          <label htmlFor="date">
+            <input type="text" name="date" placeholder="dates"></input>
+          </label>
+          <button type="button">valider</button>
+        </form>
+      </div>
     </div>
   );
 };
