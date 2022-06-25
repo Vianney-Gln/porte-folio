a53a475c0f8a32807e9bbd700bf64017fc0976f2
Vianney-Gln <vgeloen@gmail.com>
2022-06-25 16:44:07 +0200

    style css ok

--- src/admin/admin.jsx
+++ src/admin/admin.jsx
@@ -6,7 +6,9 @@ import uploadPhoto from "../service/service";
 
 const Admin = () => {
   const [file, setFile] = useState(null); // state to send file
-  const [message, setMessage] = useState(""); // state manage messages
+  const [messagePhoto, setMessagePhoto] = useState(""); // state manage messages
+  const [messageIntro, setMessageIntro] = useState(""); // state manage messages
+  const [messageProject, setMessageProject] = useState(""); // state manage messages
   /**
    * Function running the service uploadPhoto function
    * @param {event} e
@@ -16,15 +18,26 @@ const Admin = () => {
     data.append("file", file);
     uploadPhoto(data)
       .then(() => {
-        setMessage("la photo est modifiée");
+        setMessagePhoto("la photo est modifiée");
       })
-      .catch(() => setMessage("photo non envoyée"));
+      .then(() => {
+        setTimeout(() => {
+          window.location.reload();
+        }, 3000);
+      })
+      .catch(() => setMessagePhoto("photo non envoyée"));
   };
   return (
     <div className="admin">
       <h1>Administration du portfolio</h1>
       <div className="container-form">
         <form className="form-photo">
+          <div className="container-avatar">
+            <img
+              src="http://localhost:3001/api/portFolio_Vianney/upload"
+              alt="avatar"
+            />
+          </div>
           <h2>Changer la photo de l'introduction</h2>
           <div className="change-button">
             <label htmlFor="file">
@@ -38,44 +51,52 @@ const Admin = () => {
               changer la photo
             </button>
           </div>
-          {message && <p>{message}</p>}
+          <p className="message">{messagePhoto ? messagePhoto : ""}</p>
         </form>
         <form className="form-text-intro">
-          <h2>Modification du texte d'introduction ou de actuellement </h2>
+          <h2>Modification du texte d'introduction ou de actuellement</h2>
           <label htmlFor="introduction">
+            <span>Texte d'introduction: </span>
             <textarea placeholder="introduction" name="introduction"></textarea>
           </label>
           <label htmlFor="actually">
+            <span>Actuellement: </span>
             <input type="text" name="actually" placeholder="actually"></input>
           </label>
           <button type="button">valider</button>
+          <p className="message">{messageIntro ? messageIntro : ""}</p>
         </form>
         <form className="form-projects">
           <h2>Gestion des projets</h2>
           <label htmlFor="name">
+            <span>Nom du projet: </span>
             <input type="text" name="name" placeholder="nom du projet"></input>
           </label>
           <label htmlFor="url">
+            <span>lien vers le site: </span>
             <input type="text" name="url" placeholder="url du site"></input>
           </label>
           <label htmlFor="image-url">
+            <span>image url du projet </span>
             <input
               type="text"
               name="image-url"
               placeholder="url image du projet"
             ></input>
           </label>
-          <label htmlFor="description">
-            <input
-              type="text"
-              name="description"
-              placeholder="description du projet"
-            ></input>
-          </label>
           <label htmlFor="date">
+            <span>Périodes: </span>
             <input type="text" name="date" placeholder="dates"></input>
           </label>
+          <label htmlFor="description">
+            <span>Description du projet </span>
+            <textarea
+              name="description"
+              placeholder="description du projet"
+            ></textarea>
+          </label>
           <button type="button">valider</button>
+          <p className="message">{messageProject ? messageProject : ""}</p>
         </form>
       </div>
     </div>
