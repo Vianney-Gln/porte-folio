54a8d8e228f047f8533abb05cdcd6fe7cca32384
Vianney-Gln <vgeloen@gmail.com>
2022-06-27 14:11:58 +0200

    upgrade css manage projects and display a message if no projects

--- src/admin/admin.jsx
+++ src/admin/admin.jsx
@@ -208,20 +208,31 @@ const Admin = () => {
           </button>
           <p className="message">{messageProject ? messageProject : ""}</p>
         </form>
-        <h2>Gestion des projets</h2>
+        <h2 className="title-manage-project">Gestion des projets</h2>
         <div className="manage-projects">
           <div className="container-projects">
-            <ul className="list-projects">
-              {contextInfos.projects.length
-                ? contextInfos.projects.map((project) => {
-                    return (
-                      <li key={project.id}>
-                        <h2>{project.name}</h2>
-                      </li>
-                    );
-                  })
-                : ""}
-            </ul>
+            {contextInfos.projects.length ? (
+              <ul className="list-projects">
+                {contextInfos.projects.length
+                  ? contextInfos.projects.map((project) => {
+                      return (
+                        <li key={project.id}>
+                          <h3>{project.name}</h3>
+                          <img
+                            src={`http://localhost:3001/api/portFolio_Vianney/projects/image/${project.id}`}
+                            alt={project.name}
+                          />
+                          <button type="button">Supprimer</button>
+                        </li>
+                      );
+                    })
+                  : ""}
+              </ul>
+            ) : (
+              <p className="no-projects">
+                Vous n'avez pas encore réalisé de projets...
+              </p>
+            )}
           </div>
         </div>
       </div>
