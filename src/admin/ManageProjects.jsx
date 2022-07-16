056527c03df6440e2706e1c6c430a7d139253a5e
Vianney-Gln <vgeloen@gmail.com>
2022-07-16 17:09:11 +0200

    getting image direct from api in base 64 format in manageProjects components

--- src/admin/ManageProjects.jsx
+++ src/admin/ManageProjects.jsx
@@ -19,7 +19,7 @@ const ManageProjects = ({ setToUpdate, setModalOpen, setIdProject }) => {
                       <li key={project.id}>
                         <h3>{project.name}</h3>
                         <img
-                          src={`https://portfolio-vianney.herokuapp.com/api/portFolio_Vianney/projects/image/${project.id}`}
+                          src={`data:${project.type};base64,${project.base64}`}
                           alt={project.name}
                         />
                         <div className="buttons">
